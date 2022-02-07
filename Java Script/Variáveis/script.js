a2462b6cf14d50bf67b84ca63d16d7a297b6c6df
xArthur29 <arthurgruimaraes@gmail.com>
2022-02-07 10:44:10 -0300

    variaáveis

---
+++ Java Script/Variáveis/script.js
@@ -0,0 +1,14 @@
+//string
+var texto = "Curso JavaScript"
+
+//number
+var numeroInteiro = -7
+var numeroFracionado = 1255.68
+
+//boolean
+var verdadeira = true
+var falsa = false
+
+//alert(numeroFracionado) //abrir um dialog
+//document.write(texto)
+console.log(numeroInteiro)
